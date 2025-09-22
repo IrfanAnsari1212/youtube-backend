afd1401189b250699ad04191d7369e4e47135f8b
Irfan Ansari <151234719+IrfanAnsari1212@users.noreply.github.com>
2025-09-22 13:25:14 +0530

    Implement code changes to enhance functionality and improve performance

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -254,9 +254,9 @@ const logoutUser = asyncHandler(async (req, res) => {
   User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: {
-        refreshToken: undefined,
-      },
+      $unset: {
+        refreshToken: 1,// this remove the field from document
+      }
     },
     {
       new: true,
